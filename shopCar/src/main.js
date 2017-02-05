139d92f744de4916e2482d7ca3356ad7b85eaee6
k-water <625592890@qq.com>
2017-02-06 00:14:29 +0800

    静态页面初始化

--- shopCar/src/main.js
+++ shopCar/src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App'
-
+import VueResource from 'vue-resource'
 /* eslint-disable no-new */
+Vue.use(VueResource)
 new Vue({
   el: '#app',
   render: h => h(App)
